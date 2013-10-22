cc10bea8c562024d10e859e3318ad3958916be60
Christopher J. Volny <cvolny@gmail.com>
2013-10-22 19:16:02 -0400

    fixed for assignment requirements (hold b, m for test cases)

--- profile_powermod.py
+++ profile_powermod.py
@@ -16,27 +16,26 @@ from volny_magic import random_length_pair, random_length
 from discrete import powermod, rpowermod
 """
 
-rpowermodstmt = """
-a, b = random_length_pair({0})
-rpowermod(a, b, random_length({1}))
+rcode = """
+rpowermod(random_length({0}), {1}, {2})
 """
 
-ipowermodstmt = """
-a, b = random_length_pair({0})
-powermod(a, b, random_length({1}))
+icode = """
+powermod(random_length({0}), {1}, {2})
 """
 
 
 def main():
-    inter = 1000
+    b, m = 100, 200
+    inter = 10000
     cases = [x*100 for x in range(1,11)]
     width = len(str(max(cases)))
     with open(os.path.expanduser("~/Desktop/powermod_results.csv"), 'wbc') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(('i','rpowermod','ipowermod'))
+        writer.writerow(('N-Length', 'Recursive', 'Iterative'))
         for case in cases:
-            rstmt = rpowermodstmt.format(case, case)
-            istmt = ipowermodstmt.format(case, case)
+            rstmt = rcode.format(case, b, m)
+            istmt = icode.format(case, b, m)
             r = median(timeit.Timer(rstmt, setup=setupstmt).repeat(inter, 1))
             i = median(timeit.Timer(istmt, setup=setupstmt).repeat(inter, 1))
             indx = ('{0:%sd}' % (width,)).format(case)
