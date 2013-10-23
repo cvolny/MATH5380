33e1dc61f0970c5ad87b460b06a2d0e82868e268
Christopher J. Volny <cvolny@gmail.com>
2013-10-22 21:05:44 -0400

    cleaned up to match what I've done in profile_egcd.py.

--- profile_gcd.py
+++ profile_gcd.py
@@ -1,76 +1,41 @@
+import csv
 import logging
 import os
-import random
 import sys
 import timeit
-from volny_magic import median, random_length #, tosn
-from discrete import count_gcd
+from volny_magic import median
+
 
 """
-This module will profile GCD and write out to CSVs for further analysis in LibreOffice Calc.
-
-I don't use magic here, I use the actual timeit module (so capturing and processing results are quicker.
-
-TODO:
-    - further debug randint() errors for 2nd tests (see random_length() comment)
-    - add matplotlib support to eliminate 3rd party spreadsheet
-    - convert to numpy functions
-    - cleanup logging facility
+This module will profile gcd and write the results to csv
 """
 
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
+setupstmt = """
+from volny_magic import random_length_pair, random_length
+from discrete import egcd, regcd
+"""
+
+icode = """
+egcd(*random_length_pair({0}))
+"""
+
 
 def main():
-    cases = [j*100 for j in range(1,11)]
-    results = []
-
-    # repeat 10k times and grab minimum runtimes. Track random gen separate from gcd time.
-    for i in cases:
-        statement = '10**{}'.format(i)
-        t1 = min(timeit.Timer(statement).repeat(10000,1))
-        setup = '''
-import random
-low = 10**{0}
-high = 10**{1}-1
-    '''.format(i, i+1)
-        statement = 'random.randint(low, high)'
-        t2 = min(timeit.Timer(statement, setup=setup).repeat(10000, 1))
-        setup += '''
-from main import gcd
-a = {0}
-b = {0}
-    '''.format(statement,)
-        statement = 'gcd(a, b)'
-        t3 = min(timeit.Timer(statement, setup=setup).repeat(10000, 1))
-        r = [str(x) for x in (i, t2, t3,)]
-        results.append(",".join(r))
-
-
-    with open(os.path.expanduser("~/Desktop/gcd_results.csv"), "w") as f:
-        f.write("i,random,gcd\n")
-        f.write("\n".join(results))
-
-    results = []
-
-    # Run 100 subcases per case value and grab the median count.
-    for i in cases:
-        subcases = range(100)
-        subcases_count = len(subcases)
-        counts = [1]*subcases_count
-        for j in subcases:
-            # use random_length(i) here to avoid low entropy errors on Intel Core2
-                # needs further investigation to why above method won't work here
-            a = random_length(i)
-            b = random_length(i)
-            c = count_gcd(a,b)
-            #logging.debug('count_gcd(%s,%s) = %d' % (tosn(a), tosn(b), c,))
-            subcases[j] = c
-        results.append('%d,%s' % (i, str(median(subcases)),))
-
-    with open(os.path.expanduser("~/Desktop/gcd_count_results.csv"), "w") as f:
-        f.write("i,count\n")
-        f.write("\n".join(results))
+    inter = 10000
+    cases = [x*100 for x in range(1,11)]
+    width = len(str(max(cases)))
+    with open(os.path.expanduser("~/Desktop/gcd_results.csv"), 'wbc') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(('N-Length', 'GCD'))
+        for case in cases:
+            istmt = icode.format(case,)
+            i = median(timeit.Timer(istmt, setup=setupstmt).repeat(inter, 1))
+            indx = ('{0:%sd}' % (width,)).format(case)
+            istr = '{0:0.15f}'.format(i)
+            writer.writerow([indx, istr])
+            csvfile.flush()
 
 if "__main__" == __name__:
     main()
